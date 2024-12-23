b86491ce22b384718196a9fe463b7f7bc3ba7612
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2024-12-23 23:00:36 +0530

    create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const messageInput =document.querySelector(".message-input");
+
+const createMessageElement= (content, classes) => {
+    const div =document.createElement(".div");
+    div.classList.add("message", classes);
+    div.innerHTML =content;
+    return div;
+}
+const handleOutgoingMessage =(userMessage) => {
+    const messageContent = '<div class="message-text">${userMessage}</div}';
+    createMessageElement(messageContent,"user-message")
+}
+
+messageInput.addEventListener("keydown", (e)=>{
+    const userMessage = e.target.value.trim();
+    if(e.key === "Enter" && userMessage){
+        handleOutgoingMessage(userMessage);
+    }
+})
