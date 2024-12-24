d20a941103d330ba0a78eef8f02a89e570674626
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2024-12-24 18:32:13 +0530

    UPDATE scriptnew.js

--- scriptnew.js
+++ scriptnew.js
@@ -8,8 +8,8 @@ const chatbotToggler =document.querySelector("#chatbot-Toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 
-const API_KEY = "AIzaSyCRbanx_Z-tdI8tKvTw7JqAZlVNebP9MMc";
-const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}';
+const API_KEY = "AIzaSyDq2kQuUN5KmzmWhNW3XlqHHvv68nVUCsw";
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
      message: null,
@@ -37,8 +37,8 @@ chatHistory.push({
     role: "user",
     parts:[{text: userData.message },...(userData.file.data ?[{inline_data: userData.file}]:
         [])]
-
 });
+
 
     const requestOptions = {
         method: "POST",
@@ -74,17 +74,36 @@ try{
 }
 }
 
-setTimeout(()=>{
-    const messageContent = `<svg class="bot-avatar" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1024 1024">
-            <path d="M738.3 287.6H285.7c-59 0-106.8 47.8-106.8 106.8v303.1c0 59 47.8 106.8 106.8 106.8h81.5v111.1c0 .7.8 1.1 1.4.7l166.9-110.6 41.8-.8h117.4l43.6-.4c59 0 106.8-47.8 106.8-106.8V394.5c0-59-47.8-106.9-106.8-106.9zM351.7 448.2c0-29.5 23.9-53.5 53.5-53.5s53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5-53.5-23.9-53.5-53.5zm157.9 267.1c-67.8 0-123.8-47.5-132.3-109h264.6c-8.6 61.5-64.5 109-132.3 109zm110-213.7c-29.5 0-53.5-23.9-53.5-53.5s23.9-53.5 53.5-53.5 53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5zM867.2 644.5V453.1h26.5c19.4 0 35.1 15.7 35.1 35.1v121.1c0 19.4-15.7 35.1-35.1 35.1h-26.5zM95.2 609.4V488.2c0-19.4 15.7-35.1 35.1-35.1h26.5v191.3h-26.5c-19.4 0-35.1-15.7-35.1-35.1zM561.5 149.6c0 23.4-15.6 43.3-36.9 49.7v44.9h-30v-44.9c-21.4-6.5-36.9-26.3-36.9-49.7 0-28.6 23.3-51.9 51.9-51.9s51.9 23.3 51.9 51.9z"></path>
-        </svg>
-        <div class="message-text">Hey There <br>How can I help You?</div>`;
+const handleOutgoingMessage = (e) =>{
+    e.preventDefault();
+    userData.message = messageInput.value,trim();
+    messageInput.value = "";
+    fileUploadWrapper.classList.remove("file-uploaded")
 
-    const incomingMessageDiv = createMessageElement(messageContent, "bot-message", "thinking");
-    chatBody.appendChild(incomingMessageDiv);
-    chatBody.scrollTo({top:chatBody.scrollHeight, behavior:"smooth"});
-    generateBotResponse(incomingMessageDiv);
-}, 600);
+    const messageContent = `<div class="message-text"></div>
+                            ${userData.file.data ? `<img src ="data: ${userData.file.mime_type};
+                                base64,${userData.file.data}" class="attachment" />` :""}`;
+
+    const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+    outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
+    chatBody.appendChild(outgoingMessageDiv);
+    chatBody.scrollTo({top:chatBody.scrollHeight,behavior: "smooth"});
+
+
+    setTimeout(()=>{
+        const messageContent = `<svg class="bot-avatar" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1024 1024">
+                <path d="M738.3 287.6H285.7c-59 0-106.8 47.8-106.8 106.8v303.1c0 59 47.8 106.8 106.8 106.8h81.5v111.1c0 .7.8 1.1 1.4.7l166.9-110.6 41.8-.8h117.4l43.6-.4c59 0 106.8-47.8 106.8-106.8V394.5c0-59-47.8-106.9-106.8-106.9zM351.7 448.2c0-29.5 23.9-53.5 53.5-53.5s53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5-53.5-23.9-53.5-53.5zm157.9 267.1c-67.8 0-123.8-47.5-132.3-109h264.6c-8.6 61.5-64.5 109-132.3 109zm110-213.7c-29.5 0-53.5-23.9-53.5-53.5s23.9-53.5 53.5-53.5 53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5zM867.2 644.5V453.1h26.5c19.4 0 35.1 15.7 35.1 35.1v121.1c0 19.4-15.7 35.1-35.1 35.1h-26.5zM95.2 609.4V488.2c0-19.4 15.7-35.1 35.1-35.1h26.5v191.3h-26.5c-19.4 0-35.1-15.7-35.1-35.1zM561.5 149.6c0 23.4-15.6 43.3-36.9 49.7v44.9h-30v-44.9c-21.4-6.5-36.9-26.3-36.9-49.7 0-28.6 23.3-51.9 51.9-51.9s51.9 23.3 51.9 51.9z"></path>
+            </svg>
+            <div class="message-text">Hey There <br>How can I help You?</div>`;
+    
+        const incomingMessageDiv = createMessageElement(messageContent, "bot-message", "thinking");
+        chatBody.appendChild(incomingMessageDiv);
+        chatBody.scrollTo({top:chatBody.scrollHeight, behavior:"smooth"});
+        generateBotResponse(incomingMessageDiv);
+    }, 600);
+}
+
+
 
 messageInput.addEventListener("keydown", (e)=>{
     const userMessage = e.target.value.trim();
@@ -92,6 +111,15 @@ messageInput.addEventListener("keydown", (e)=>{
         handleOutgoingMessage(0);
     }
 });
+
+
+messageInput.addEventListener("input", () => {
+    messageInput.style.height = `${initialInputHeight}px`;
+    messageInput.style.height = `${messageInput.scrollHeight}px`;
+    document.querySelector(".chat-form").style.borderRadius =messageInput.scrollHeight > 
+    initialInputHeight ? "15px" : "32px";
+});
+
 
 fileInput.addEventListener("change",() => {
     const file = fileInput.file[0];
@@ -103,10 +131,15 @@ fileInput.addEventListener("change",() => {
         fileUploadWrapper.classList.add("file-uploaded");
         const base64string = e.target.result.split(",")[1];
 
+        userData.file = {
+            data:base64string,
+            mime_type:file.type
+        }
         fileInput.value="";
     }
     reader.readAsDataURL(file);
 });
+
 
 fileCancelButton.addEventListener("click", () =>{
     userData.file= {};
