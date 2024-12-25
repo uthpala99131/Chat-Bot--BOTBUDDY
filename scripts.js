0a569214a3e74c7b7b1bc4b5c7ff402e25ac82e4
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2024-12-25 09:24:05 +0530

    update. script.js

--- scripts.js
+++ scripts.js
@@ -185,3 +185,34 @@ chatbotToggler.addEventListener("click", () => document.body.classList.toggle("s
 closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
